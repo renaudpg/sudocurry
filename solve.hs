b2c0c9cec3ae6440d60ab738a8d4873891364b62
Perry <persummer@gmail.com>
2016-11-21 00:55:08 -0500

    Added legal move check

--- solve.hs
+++ solve.hs
@@ -1,7 +1,8 @@
 import Data.Map
 import Data.List
+import Data.Maybe (fromJust)
 
-main = print $ firstEmpty ((replicate 8 (replicate 9 1)) ++ [[1,0,1,1,1,1,1,1,1]])
+main = print $ firstEmpty (replicate 9 (replicate 9 0))
 
 gRange = [0..8]
 
@@ -20,9 +21,15 @@ peers x y = [tup | tup<-(nub $ colList x ++ rowList y ++ secList x y), tup /= (x
 
 peerMap = fromList [((x, y), peers x y) | x<-gRange, y<-gRange]
 
+head' :: [a] -> Maybe a
+head' [] = Nothing
+head' (x:xs) = Just x
 
 getItem :: [[a]] -> (Int, Int) -> a
 getItem xs (x, y) = xs!!y!!x
 
+firstEmpty :: [[Int]] -> Maybe (Int, Int)
+firstEmpty grid = head' [(x, y) | x<-gRange, y<-gRange, (getItem grid (x, y)) == 0]
 
-firstEmpty grid = [(x, y) | x<-gRange, y<-gRange, (getItem grid (x, y)) == 0]
+isLegal :: [[Int]] -> (Int, Int) -> Int -> Bool
+isLegal xs (x, y) n = all (\k -> k /= n) [getItem xs (i, j) | (i, j)<-fromJust(Data.Map.lookup (x, y) peerMap)]
